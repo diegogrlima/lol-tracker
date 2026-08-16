c0aa2f0b89420dfb58cd6ed161fba456fce75fbc
Diego Lima <diegolimareis021@gmail.com>
2026-08-16 08:53:30 -0400

    feat(api): add graceful server shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,33 +2,41 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/diegogrlima/lol-tracker/internal/application"
 	"github.com/diegogrlima/lol-tracker/internal/database"
 )
 
 func main() {
-	ctx := context.Background()
-	redisAddress := os.Getenv("REDIS_ADDRESS")
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
 
+	redisAddress := os.Getenv("REDIS_ADDRESS")
 	if redisAddress == "" {
 		redisAddress = "localhost:6379"
 	}
 
 	redisClient, err := database.NewRedis(ctx, redisAddress)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalf("connect to Redis: %v", err)
 	}
-
 	defer redisClient.Close()
-	fmt.Println("Conectado ao Redis")
 
 	app := application.New(redisClient)
 
+	log.Println("API started on port 8080")
+
 	if err := app.Start(ctx); err != nil {
-		fmt.Println("Erro ao iniciar o servidor:", err)
+		log.Fatalf("run application: %v", err)
 	}
+
+	log.Println("API stopped")
 }
--- internal/application/app.go
+++ internal/application/app.go
@@ -2,8 +2,10 @@ package application
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -14,24 +16,40 @@ type App struct {
 }
 
 func New(redisClient *redis.Client) *App {
-	app := &App{
+	return &App{
 		router:      loadRoutes(),
 		redisClient: redisClient,
 	}
-
-	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr:    ":8080",
-		Handler: a.router,
+		Addr:              ":8080",
+		Handler:           a.router,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
+	serverError := make(chan error, 1)
 
-		return fmt.Errorf("Erro ao iniciar o servidor: %w", err)
+	go func() {
+		serverError <- server.ListenAndServe()
+	}()
+
+	select {
+	case err := <-serverError:
+		if !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("start HTTP server: %w", err)
+		}
+	case <-ctx.Done():
+		shutdownCtx, cancel := context.WithTimeout(
+			context.Background(),
+			10*time.Second,
+		)
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			return fmt.Errorf("shutdown HTTP server: %w", err)
+		}
 	}
 
 	return nil
